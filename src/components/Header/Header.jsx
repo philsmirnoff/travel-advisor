543ad777a69bfd36742a06de3d760cb8df4f4dc5
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-12-15 21:45:44 -0500

    added google maps

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,9 +20,9 @@ const Header = () => {
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
+            </div>
               <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
-          </div>
         {/* </Autocomplete> */}
         </Box>
       </Toolbar>
